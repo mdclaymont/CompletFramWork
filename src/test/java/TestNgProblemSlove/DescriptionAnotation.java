7dd25da9df98de88858f81b38666221a1603d403
nyc <nyc@192.168.0.15>
2021-01-18 19:44:06 -0500

    update

--- src/test/java/TestNgProblemSlove/DescriptionAnotation.java
+++ src/test/java/TestNgProblemSlove/DescriptionAnotation.java
@@ -11,52 +11,42 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
-
 import Common.BaseClass;
 import Utilities.TestListeners;
+
 @Listeners(TestListeners.class)
 public class DescriptionAnotation extends BaseClass {
 	@BeforeTest
-	//@Parameters("browserName")
+
 	public void setUp() throws IOException {
 		System.out.println("SetUp");
-		//getData("CrigList,4");
-		System.out.println(browserName);
-		//initilizeDriver("");
-		//openUrl("");
 	}
-	@Test(description="This Is Test For Description")
-	
+
+	@Test(description = "This Is Test For Description")
+
 	public void Description() {
-	
-	//	softAssert().assertEquals(false,true);
 		System.out.println("This Is Description Method");
 	}
-	
-	@Test(description="This Is Parameter Test")
-	@Parameters({"browser","Url"})
-	public void Parameter(String browser,String Url) {
+
+	@Test(description = "This Is Parameter Test")
+	@Parameters({ "browser", "Url" })
+	public void Parameter(String browser, String Url) {
 		System.out.println(browser);
 		System.out.println(Url);
-		System.out.println("postIphone(");
-		//TestListener.test.log(Status.FAIL,"Exptest Test Pass Failed");	
 	}
-	@Test(groups= {"Save"})
+
+	@Test(groups = { "Save" })
 	public void Testc1() {
 		System.out.println("Test1");
-		softAssert().assertEquals(false,true);
 	}
-	
+
 	@Test
 	public void postIphone1() {
 		System.out.println("postIphone1(");
-		//TestListener.test.log(Status.FAIL,"Exptest Test Pass Failed");	
 	}
+
 	@AfterTest
 	public void tearDown() {
 		System.out.println("tear");
-		softAssert().assertAll("All Result");
 	}
-
 }
